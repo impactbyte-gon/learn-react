7c774d21b8a999b121535eba1a471e1d00803cfe
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-12 14:42:33 +0700

    Refactor state from TaskList to Content

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -4,11 +4,40 @@ import TaskForm from '../TaskForm'
 import TaskList from '../TaskList'
 
 class Content extends Component {
+  constructor() {
+    super()
+    this.state = {
+      data: [
+        {
+          text: 'Running a marathon'
+        },
+        {
+          text: 'Eating a lunch'
+        },
+        {
+          text: 'Drinking a water'
+        }
+      ]
+    }
+  }
+
+  addTask = (event, inputText) => {
+    event.preventDefault()
+
+    const newData = this.state.data.concat({
+      text: inputText
+    })
+
+    this.setState({
+      data: newData
+    })
+  }
+
   render() {
     return (
       <React.Fragment>
-        <TaskForm />
-        <TaskList />
+        <TaskForm addTask={this.addTask} />
+        <TaskList data={this.state.data} />
       </React.Fragment>
     )
   }
--- src/components/TaskList/index.js
+++ src/components/TaskList/index.js
@@ -3,27 +3,10 @@ import React, { Component } from 'react'
 import Task from '../Task'
 
 class TaskList extends Component {
-  constructor() {
-    super()
-    this.state = {
-      data: [
-        {
-          text: 'Running a marathon'
-        },
-        {
-          text: 'Eating a lunch'
-        },
-        {
-          text: 'Drinking a water'
-        }
-      ]
-    }
-  }
-
   render() {
     return (
       <ul>
-        {this.state.data.map((item, index) => {
+        {this.props.data.map((item, index) => {
           return <Task key={index}>{item.text}</Task>
         })}
       </ul>
