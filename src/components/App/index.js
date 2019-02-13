3c835675648006f68e263472aeb6606d2a6a2956
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-13 15:00:55 +0700

    Style Container with styled-components

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 
 import Header from '../Header'
 import Content from '../Content'
 import Footer from '../Footer'
 
+const Container = styled.div`
+  width: 300px;
+  height: 80vh;
+  margin: 0 auto;
+  border: 2px solid black;
+`
+
 class App extends Component {
   render() {
     return (
-      <div>
+      <Container>
         <Header />
         <Content />
         <Footer />
-      </div>
+      </Container>
     )
   }
 }
